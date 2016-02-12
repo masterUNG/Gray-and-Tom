ef545a8f465d99e473cf5baa22cbf3944128268e
masterUNG <phrombutr@gmail.com>
2016-02-12 13:35:59 +0700

    Add User Finish

--- app/src/main/java/com/example/rsu/myproject/LoginActivity.java
+++ app/src/main/java/com/example/rsu/myproject/LoginActivity.java
@@ -5,9 +5,25 @@ import android.os.Bundle;
 
 public class LoginActivity extends AppCompatActivity {
 
+    //Explicit
+    private MyManage objMyManage;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        //Connected Database
+        objMyManage = new MyManage(this);
+
+        //Test Add Value
+        testAddValue();
+
+
+    }   // Main Method
+
+    private void testAddValue() {
+        objMyManage.addUser("user", "pass", "name");
     }
-}
+
+}   // Main Class
--- app/src/main/java/com/example/rsu/myproject/MyManage.java
+++ app/src/main/java/com/example/rsu/myproject/MyManage.java
@@ -1,5 +1,6 @@
 package com.example.rsu.myproject;
 
+import android.content.ContentValues;
 import android.content.Context;
 import android.database.sqlite.SQLiteDatabase;
 
@@ -12,6 +13,20 @@ public class MyManage {
     private MyOpenHelper objMyOpenHelper;
     private SQLiteDatabase writeSqLiteDatabase, readSqLiteDatabase;
 
+    public static final String table_user = "userTABLE";
+    public static final String table_major = "majorTABLE";
+    public static final String column_id = "_id";
+    public static final String column_User = "User";
+    public static final String column_Password = "Password";
+    public static final String column_Name = "Name";
+    public static final String column_Category = "Category";
+    public static final String column_NameMajor = "NameMajor";
+    public static final String column_Web = "Web";
+    public static final String column_Image = "Image";
+    public static final String column_Detail = "Detail";
+    public static final String column_Lat = "Lat";
+    public static final String column_Lng = "Lng";
+
 
     public MyManage(Context context) {
 
@@ -21,5 +36,18 @@ public class MyManage {
         readSqLiteDatabase = objMyOpenHelper.getReadableDatabase();
 
     }   // Constructor
+
+    public long addUser(String strUser,
+                        String strPassword,
+                        String strName) {
+
+        ContentValues objContentValues = new ContentValues();
+        objContentValues.put(column_User, strUser);
+        objContentValues.put(column_Password, strPassword);
+        objContentValues.put(column_Name, strName);
+
+        return writeSqLiteDatabase.insert(table_user, null, objContentValues);
+    }
+
 
 } // Main Class
